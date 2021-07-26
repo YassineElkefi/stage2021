cc10baebaa389c7fe5f155ad6f46e2eb9c6b3f89
YassineElkefi <yassine.elkefi6@gmail.com>
2021-07-26 22:17:52 +0100

    VBE

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,3 @@
-from re import MULTILINE
 from typing import Optional
 from fastapi import FastAPI , Request
 from fastapi.params import Body
@@ -6,6 +5,11 @@ import mysql.connector
 import json
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse
+from reportlab.pdfgen import canvas
+from starlette.responses import FileResponse
+from fastapi.responses import StreamingResponse
+import csv
+
 app = FastAPI()
 
 origins = [
@@ -122,4 +126,30 @@ async def db_test(request : Request):
     json_data=[]
     for result in rv:
             json_data.append(dict(zip(row_headers,result)))
+    return json_data
+
+####################################################################################################################
+#SELECT * FROM depot where DATE(date_envoi)  BETWEEN     '2021-09-07'   AND '2021-12-07'
+@app.get("/selectdepot")
+def search(Client :str, bp:str, service :str, date_s :str, date_e:str):
+    db = mysql.connector.connect(host = "localhost" , user = "root" , password = "" , database = "stage2021")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT * FROM depot where client='{Client}' and bureau='{bp}' and service='{service}' and CAST(date_envoi AS date) BETWEEN '{date_s}' AND '{date_e}'")
+    row_headers=[x[0] for x in cursor.description]
+    rv = cursor.fetchall()
+    json_data=[]
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+    return json_data
+#####################################################################################################################
+@app.get("/selectall")
+def search():
+    db = mysql.connector.connect(host = "localhost" , user = "root" , password = "" , database = "stage2021")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT * FROM depot")
+    row_headers=[x[0] for x in cursor.description]
+    rv = cursor.fetchall()
+    json_data=[]
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
     return json_data
